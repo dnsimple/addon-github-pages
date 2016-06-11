720b80789b0c46489c43f6ecb0b8ca28d4124bbe
Anthony Eden <me@anthonyeden.com>
2016-06-11 21:19:54 +0200

    The database create can remain.
    The migrate can be uncommented once there is at least
    one migration.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,5 @@
 ExUnit.start
 
-# Mix.Task.run "ecto.create", ~w(-r GithubPagesConnector.Repo --quiet)
+Mix.Task.run "ecto.create", ~w(-r GithubPagesConnector.Repo --quiet)
 # Mix.Task.run "ecto.migrate", ~w(-r GithubPagesConnector.Repo --quiet)
 Ecto.Adapters.SQL.Sandbox.mode(GithubPagesConnector.Repo, :manual)
