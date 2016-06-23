6c481b9b9400c083af7381c6b42dd7b2018a7c88
Javier Acero <j4cegu@gmail.com>
2016-06-23 18:11:48 +0200

    Store the account

--- web/controllers/github_oauth_controller.ex
+++ web/controllers/github_oauth_controller.ex
@@ -1,5 +1,7 @@
 defmodule GithubPagesConnector.GithubOauthController do
   use GithubPagesConnector.Web, :controller
+  alias GithubPagesConnector.Account
+  alias GithubPagesConnector.MemoryRepo
 
   @client_id "26bdee190f3d6af8f9e3"
   @client_secret "a5f8fb3be0467f30a34f0f574c2c7cd9877463a5"
@@ -20,11 +22,18 @@ defmodule GithubPagesConnector.GithubOauthController do
   def create(conn, params) do
     dnsimple_account_id   = get_session(conn, :dnsimple_account_id)
     dnsimple_access_token = get_session(conn, :dnsimple_access_token)
-    github_access_token   = OAuth2.Client.get_token!(@client, code: params["code"])
+    github_access_token   = OAuth2.Client.get_token!(@client, code: params["code"]).access_token
+
+    MemoryRepo.put(dnsimple_account_id, %Account{
+      dnsimple_account_id: dnsimple_account_id,
+      dnsimple_access_token: dnsimple_access_token,
+      github_access_token: github_access_token,
+    })
+
     render(conn, "welcome.html", [
       dnsimple_account_id: dnsimple_account_id,
       dnsimple_access_token: dnsimple_access_token,
-      github_access_token: github_access_token.access_token,
+      github_access_token: github_access_token,
     ])
   end
 
