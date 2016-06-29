6bccd46f68fe24279d5a5280edf4496df22ffa67
Javier Acero <j4cegu@gmail.com>
2016-06-29 16:38:40 +0200

    Wrap tests in describe blocks for each endpoint

--- test/controllers/dnsimple_oauth_controller_test.exs
+++ test/controllers/dnsimple_oauth_controller_test.exs
@@ -1,14 +1,18 @@
 defmodule GithubPagesConnector.DnsimpleOauthControllerTest do
   use GithubPagesConnector.ConnCase
 
-  test "GET /dnsimple/authorize", %{conn: conn} do
-    conn = get conn, dnsimple_oauth_path(conn, :new)
-    assert redirected_to(conn) =~ "https://test.dnsimple.com/auth/authorize?client_id=client_id&state=state"
+  describe "GET /dnsimple/authorize" do
+    test "starts the DNSimple OAuth dance", %{conn: conn} do
+      conn = get conn, dnsimple_oauth_path(conn, :new)
+      assert redirected_to(conn) =~ "https://test.dnsimple.com/auth/authorize?client_id=client_id&state=state"
+    end
   end
 
-  test "GET /dnsimple/callback", %{conn: conn} do
-    conn = get conn, dnsimple_oauth_path(conn, :create)
-    assert redirected_to(conn) =~ github_oauth_path(conn, :new)
+  describe "GET /dnsimple/callback" do
+    test "starts the GitHub OAuth dance", %{conn: conn} do
+      conn = get conn, dnsimple_oauth_path(conn, :create)
+      assert redirected_to(conn) =~ github_oauth_path(conn, :new)
+    end
   end
 
 end
