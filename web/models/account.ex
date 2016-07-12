172e641f062254840c5a932d859ac7bb177fa48d
Javier Acero <j4cegu@gmail.com>
2016-07-12 16:44:45 +0200

    Account is now an Ecto module

--- web/models/account.ex
+++ web/models/account.ex
@@ -1,10 +1,15 @@
 defmodule GithubPagesConnector.Account do
-  defstruct [
-    :dnsimple_account_id,
-    :dnsimple_account_email,
-    :dnsimple_access_token,
-    :github_account_id,
-    :github_account_login,
-    :github_access_token,
-  ]
+  use GithubPagesConnector.Web, :model
+
+  schema "accounts" do
+    field :dnsimple_account_id, :string
+    field :dnsimple_account_email, :string
+    field :dnsimple_access_token, :string
+    field :github_account_id, :string
+    field :github_account_login, :string
+    field :github_access_token, :string
+
+    timestamps
+  end
+
 end
