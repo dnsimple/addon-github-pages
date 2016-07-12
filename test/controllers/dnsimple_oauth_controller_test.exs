64f9e3b329d1f27f3cec4b9e8b2b778ef48853f5
Javier Acero <j4cegu@gmail.com>
2016-07-12 12:34:16 +0200

    Repeating the signup will update the DNSimple data
    This is necessary to keep track of email changes or to allow access
    tokens to be updated (which will happen when the user revokes the access
    to the app but wants to use it again after some time).
    
    These tests are being done at the Controller level but it would probably
    make sense to test them from the actual domain model entry point (which
    as of now is the Accounts module).

--- test/controllers/dnsimple_oauth_controller_test.exs
+++ test/controllers/dnsimple_oauth_controller_test.exs
@@ -23,6 +23,20 @@ defmodule GithubPagesConnector.DnsimpleOauthControllerTest do
       assert account.dnsimple_access_token == "dnsimple_access_token"
     end
 
+    test "updates the account data when the account had already signed up", %{conn: conn} do
+      @repo.put("dnsimple_account_id", %GithubPagesConnector.Account{
+        dnsimple_account_id: "dnsimple_account_id",
+        dnsimple_account_email: "old_dnsimple_account_email",
+        dnsimple_access_token: "old_dnsimple_access_token",
+      })
+
+      get(conn, dnsimple_oauth_path(conn, :create))
+
+      account = @repo.get("dnsimple_account_id")
+      assert account.dnsimple_account_email == "dnsimple_account_email"
+      assert account.dnsimple_access_token == "dnsimple_access_token"
+    end
+
     test "starts the GitHub OAuth dance", %{conn: conn} do
       conn = get(conn, dnsimple_oauth_path(conn, :create))
 
