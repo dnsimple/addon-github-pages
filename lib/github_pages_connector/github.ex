ad6d9c15241e71f931ee4c13cfa4c40f3221ab89
Javier Acero <j4cegu@gmail.com>
2016-07-12 16:44:30 +0200

    Removing some warnings and minor cleanup

--- lib/github_pages_connector/github.ex
+++ lib/github_pages_connector/github.ex
@@ -10,7 +10,7 @@ defmodule GithubPagesConnector.Github do
     authorize_url: Application.get_env(:github_pages_connector, :github_authorize_uri),
   ])
 
-  def oauth_authorize_url(state: state) do
+  def oauth_authorize_url(state: _state) do
     @oauth_client
     |> OAuth2.Client.put_param(:scope, @oauth_scope)
     |> OAuth2.Client.authorize_url!
@@ -21,7 +21,7 @@ defmodule GithubPagesConnector.Github do
       access_token = access_token(code, state)
       {account_id, account_login} = account_data(access_token)
       {:ok, account_id, account_login, access_token}
-    rescue error -> error
+    rescue error ->
       {:error , error}
     end
   end
@@ -30,13 +30,13 @@ defmodule GithubPagesConnector.Github do
     try do
       repositories = Tentacat.Repositories.list_mine(client(account))
       {:ok, repositories}
-    rescue error -> error
+    rescue error ->
       {:error , error}
     end
   end
 
 
-  defp access_token(code, state) do
+  defp access_token(code, _state) do
     @oauth_client
     |> OAuth2.Client.get_token!(code: code)
     |> Map.get(:access_token)
@@ -45,10 +45,9 @@ defmodule GithubPagesConnector.Github do
   defp account_data(access_token) do
     data = Tentacat.Users.me(client(access_token))
 
-    {Map.get(data, "id"), Map.get(data, "login")}
+    {data["id"], data["login"]}
   end
 
-  defp client, do: client(nil)
   defp client(%Account{github_access_token: access_token}), do: client(access_token)
   defp client(access_token), do: Tentacat.Client.new(%{access_token: access_token})
 
