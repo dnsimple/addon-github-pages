c7ee0f0aca047b0055f7b20880effff182930e1b
Javier Acero <j4cegu@gmail.com>
2016-07-13 17:48:02 +0200

    Tidy up the view
    Sort and downcase all the domain and repository names.

--- web/views/connection_view.ex
+++ web/views/connection_view.ex
@@ -1,4 +1,17 @@
 defmodule GithubPagesConnector.ConnectionView do
   use GithubPagesConnector.Web, :view
 
+  def repository_names(repositories) do
+    repositories
+    |> Enum.map(&(Map.get(&1, "name")))
+    |> Enum.map(&String.downcase/1)
+    |> Enum.sort
+  end
+
+  def domain_names(domains) do
+    domains
+    |> Enum.map(&(Map.get(&1, :name)))
+    |> Enum.sort
+  end
+
 end
