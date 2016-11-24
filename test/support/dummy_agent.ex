a04313f0600e177ba4c2500f138afd17767afce7
Javier Acero <j4cegu@gmail.com>
2016-11-24 12:18:34 +0100

    Consider arguments in dummy stubbing
    This change was prompted from an error that wasn't caught in a test (the
    fix will come in the next commit). The problem was as simple as calling
    the method with the parameters in the wrong order. The stubbing
    supported in the dummies was super simple, and didn't consider the
    argument values, so the test passed.
    
    Since I wanted to pin down the error in a regression test before adding
    the fix I've added the capability of stubbing the return value of a
    dummy function depending on the parameter values. The required changes
    make the agents a little more complicated, but I think it's worth it.

--- test/support/dummy_agent.ex
+++ test/support/dummy_agent.ex
@@ -9,6 +9,9 @@ defmodule DummyAgent do
       def stub(function, return_value) do
         Agent.update(__MODULE__, &Map.put(&1, function, return_value))
       end
+      def stub(function, args, return_value) do
+        Agent.update(__MODULE__, &Map.put(&1, {function, args}, return_value))
+      end
 
       def calls do
         Agent.get(__MODULE__, &Map.get_lazy(&1, :calls, fn -> [] end))
@@ -21,6 +24,16 @@ defmodule DummyAgent do
 
       defp get_stubbed_value(function, default) do
         Agent.get(__MODULE__, &Map.get_lazy(&1, function, fn -> default end))
+      end
+      defp get_stubbed_value(function, args, default) do
+        stubbed_function_plus_args = Agent.get(__MODULE__, &Map.get(&1, {function, args}))
+        stubbed_function_no_args   = Agent.get(__MODULE__, &Map.get(&1, function))
+
+        case {stubbed_function_plus_args, stubbed_function_no_args} do
+          {nil, nil}    -> default
+          {result, nil} -> result
+          {nil, result} -> result
+        end
       end
 
       defp record_call(function, args) do
