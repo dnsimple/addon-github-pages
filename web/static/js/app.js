bca53bd31aa5f3b6ddc4203289d17216f334bffa
Sebastian Hermida <sebas00@gmail.com>
2016-11-24 20:17:19 +0100

    Loads the domains for typeahead via AJAX

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,6 +13,7 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 import "awesomplete"
+import $ from "jquery"
 
 // Import local files
 //
@@ -20,3 +21,5 @@ import "awesomplete"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import { DomainList } from "./domain_list"
+DomainList.init($);
