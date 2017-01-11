1778a1dd0941be914bd84631e1fe86b83d39beef
Javier Acero <j4cegu@gmail.com>
2017-01-11 09:44:17 -0500

    Skip landing page if logged in

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,43 @@
 defmodule GithubPagesConnector.PageControllerTest do
   use GithubPagesConnector.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "DNSimple"
+  @accounts GithubPagesConnector.Services.Accounts
+
+  describe "GET /" do
+    test "renders index page when not logged in", %{conn: conn} do
+      conn = get conn, "/"
+
+      assert html_response(conn, 200) =~ "DNSimple"
+    end
+
+    test "redirects to connections when logged in", %{conn: conn} do
+      conn = conn
+        |> login_account
+        |> get("/")
+
+      assert redirected_to(conn) == connection_path(conn, :index)
+    end
   end
+
+  describe "GET /login" do
+    test "starts OAuth dance when not logged in", %{conn: conn} do
+      conn = get conn, "/login"
+
+      assert redirected_to(conn) == dnsimple_oauth_path(conn, :new)
+    end
+
+    test "redirects to connections when logged in", %{conn: conn} do
+      conn = conn
+        |> login_account
+        |> get("/login")
+
+      assert redirected_to(conn) == connection_path(conn, :index)
+    end
+  end
+
+  def login_account(conn) do
+    {:ok, account} = @accounts.signup_account(dnsimple_account_id: "dnsimple_account_id")
+    assign(conn, :current_account_id, account.dnsimple_account_id)
+  end
+
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,14 +1,19 @@
 defmodule GithubPagesConnector.PageController do
   use GithubPagesConnector.Web, :controller
 
+  plug GithubPagesConnector.Plug.CurrentAccount, require_authentication: false
+
   def index(conn, _params) do
-    render conn, "index.html"
+    case conn.assigns[:current_account] do
+      nil      -> render(conn, "index.html")
+      _account -> redirect(conn, to: connection_path(conn, :index))
+    end
   end
 
   def login(conn, _params) do
     case conn.assigns[:current_account] do
       nil      -> redirect(conn, to: dnsimple_oauth_path(conn, :new))
-      _account -> redirect(conn, to: connection_path(conn, :new))
+      _account -> redirect(conn, to: connection_path(conn, :index))
     end
   end
 
